d5c0bfbdfd3d71464e0d6c0b8f9bd2a273c9e0c2
kasamalimukhi <kasamalimukhi003@gmail.com>
2024-06-24 11:30:44 +0530

    E-commerce website with php laravel

---
+++ E-commerce/resources/views/footer.blade.php
@@ -0,0 +1,37 @@
+<footer class="bg-dark text-white text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row">
+            <!-- Column 1 -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">About Us</h5>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel elit scelerisque mauris pellentesque pulvinar.
+                </p>
+            </div>
+            <!-- Column 2 -->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Quick Links</h5>
+                <ul class="list-unstyled mb-0">
+                    <li><a href="#!" class="text-white">Home</a></li>
+                    <li><a href="#!" class="text-white">About</a></li>
+                    <li><a href="#!" class="text-white">Services</a></li>
+                    <li><a href="#!" class="text-white">Contact</a></li>
+                </ul>
+            </div>
+            <!-- Column 3 -->
+            <div class="col-lg-4 col-md-12 mb-4 mb-md-0">
+                <h5 class="text-uppercase">Contact Us</h5>
+                <p>
+                    1234 Street Name, City, State 56789
+                    <br>
+                    Email: info@example.com
+                    <br>
+                    Phone: (123) 456-7890
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2024 M-ecommerce    </div>
+</footer>
